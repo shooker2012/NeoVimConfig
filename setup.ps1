47e421c079b3699251a22875b54f3093c28ff7ed
Shooker2012 <lovelywzz@gmail.com>
2024-02-02 23:49:06 +0800

    添加了ps安装脚本

---
+++ setup.ps1
@@ -0,0 +1,6 @@
+﻿# Copy AppData
+# Copy-Item -Path .\AppData\Local\nvim $env:LOCALAPPDATA -Recurse
+
+# Copy share
+$nvim = (Get-Command neovim).Path | Split-Path
+Copy-Item -Path .\NeoVimFolder\share $nvim
